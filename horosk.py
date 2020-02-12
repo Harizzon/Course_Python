5c5ce165c791d10249f64454d811dc08abe6c9cb
Harizzon <dmitriypodgora@gmail.com>
2020-02-12 12:14:37 +0200

    Hello, added 2 first files

---
+++ horosk.py
@@ -0,0 +1,5 @@
+import random 
+times = ['In morning', 'in evening', 'at night']
+advices = ['beware', 'be careful', 'be open mind']
+promises = ['have fun', 'good luck', "don't be late"]
+generated_prophecies = []
